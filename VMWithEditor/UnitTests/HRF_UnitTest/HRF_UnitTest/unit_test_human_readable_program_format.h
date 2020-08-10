08f6e01f39ac7587fb0d2dc92974b0fea6100f5d
PAULSWINDOWS10\pacmaninbw <paul.chernick@chernicksw.com>
2020-08-10 08:19:17 -0700

    Moved common unit test functionality into common unit test code.

--- VMWithEditor/UnitTests/HRF_UnitTest/HRF_UnitTest/unit_test_human_readable_program_format.h
+++ VMWithEditor/UnitTests/HRF_UnitTest/HRF_UnitTest/unit_test_human_readable_program_format.h
@@ -1,7 +1,7 @@
 #ifndef UNIT_TEST_HUMAN_READABLE_PROGRAM_FORMAT_H
 #define UNIT_TEST_HUMAN_READABLE_PROGRAM_FORMAT_H
 // Unit tests that can be called from main.
-extern bool init_hrf_unit_tests(char* log_file_name);
+
 extern bool unit_test_hrf_duplicate_program(unsigned test_step);
 extern bool unit_test_hrf_create_program_step(unsigned test_step);
 extern bool unit_test_hrf_convert_array_program_to_linked_list(unsigned test_step);
@@ -10,5 +10,5 @@ extern bool unit_test_hrf_writing_to_file(char* file_name, unsigned test_step);
 extern bool unit_test_hrf_reading_from_file(char* file_name, unsigned test_step);
 extern bool unit_test_all_human_readable_format();
 extern void write_to_test_log_file(char* buffer);
-extern void close_hrf_unit_tests(void);
+
 #endif
