3c1b2d54bf371d47508ce0fd81e5d416d2e8affe
PAULSWINDOWS10\pacmaninbw <paul.chernick@chernicksw.com>
2020-08-11 06:10:58 -0700

    Added separate unit test for statemachine

---
+++ VMWithEditor/UnitTests/State_Machine_Unit_Test/State_Machine_Unit_Test/state_machine_unit_test_main.c
@@ -0,0 +1,12 @@
+// State_Machine_Unit_Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "common_unit_test_logic.h"
+#include "syntax_state_machine.h"
+#include <stdio.h>
+
+int main()
+{
+    printf( "Hello World!\n");
+}
+
+
