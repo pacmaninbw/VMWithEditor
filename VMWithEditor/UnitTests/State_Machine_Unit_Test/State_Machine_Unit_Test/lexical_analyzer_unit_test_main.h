b3b3d9af52f0c230a9c44d73a60fd08a27e9a94a
PAULSWINDOWS10\pacmaninbw <paul.chernick@chernicksw.com>
2020-08-31 09:01:57 -0700

    Syncing the 2 different versions of the code review.

---
+++ VMWithEditor/UnitTests/State_Machine_Unit_Test/State_Machine_Unit_Test/lexical_analyzer_unit_test_main.h
@@ -0,0 +1,6 @@
+#ifndef LEXICAL_ANALYZER_UNIT_TEST_MAIN_H
+#define LEXICAL_ANALYZER_UNIT_TEST_MAIN_H
+
+bool run_all_syntax_state_machine_unit_tests(unsigned test_step);
+
+#endif	// LEXICAL_ANALYZER_UNIT_TEST_MAIN_H
