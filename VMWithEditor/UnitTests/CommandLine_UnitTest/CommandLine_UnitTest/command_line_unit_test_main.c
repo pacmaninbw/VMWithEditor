9fd0343d10d001900bd63b77fa21cb54046d868b
PAULSWINDOWS10\pacmaninbw <paul.chernick@chernicksw.com>
2020-09-06 13:44:14 -0700

    Added include guards for command_line_unit_test_main.c, refactored includes based on changes in all other directories.

--- VMWithEditor/UnitTests/CommandLine_UnitTest/CommandLine_UnitTest/command_line_unit_test_main.c
+++ VMWithEditor/UnitTests/CommandLine_UnitTest/CommandLine_UnitTest/command_line_unit_test_main.c
@@ -1,29 +1,37 @@
-// CommandLine_UnitTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#ifndef COMMANDLINE_UNITTEST_C
+#define COMMANDLINE_UNITTEST_C
+/*
+ * CommandLine_UnitTest.c :
+ * This file contains the 'main' function. Program execution begins and ends there.
+ */
 
-#include "common_unit_test_logic.h"
-#include "arg_flags.h"
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "arg_flags.h"
+#include "error_reporting.h"
+#include "unit_test_logging.h"
 
 bool run_all_command_line_unit_tests(void)
 {
 	bool all_unit_tests_passed = true;
 
-	char buffer[BUFSIZ];
+	char buffer[LOG_BUFFER_SIZE];
 
 
 	return all_unit_tests_passed;
 }
 
-#ifndef ALL_UNIT_TESTING
+#ifdef COMMANDLINE_UNIT_TESTING_ONLY
 int main()
 {
 	error_out_file = stderr;
 	int passed = EXIT_SUCCESS;
 
-	if (!init_vm_error_reporting(NULL))
+	if (!init_vm_error_reporting(NULL) ||
+		!init_unit_tests("commandline_unittest_log.txt"))
 	{
 		return EXIT_FAILURE;
 	}
@@ -33,9 +41,11 @@ int main()
 		passed = EXIT_FAILURE;
 	}
 
+	close_unit_tests();
 	disengage_error_reporting();
 
 	return passed;
 }
-#endif
+#endif	// COMMANDLINE_UNIT_TESTING_ONLY
 
+#endif	// !COMMANDLINE_UNITTEST_C
