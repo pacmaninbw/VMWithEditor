70f8508d7d888f43eaf972e6984c5251abe723e6
PAULSWINDOWS10\pacmaninbw <paul.chernick@chernicksw.com>
2020-09-06 17:26:40 -0700

    Project files modified to reflect changes in directory architecture and to add necessary files to compile. Proprer includes added to run_all_unit_tests_main.c

--- VMWithEditor/UnitTests/RunAllUnitTests/RunAllUnitTests/run_all_unit_tests_main.c
+++ VMWithEditor/UnitTests/RunAllUnitTests/RunAllUnitTests/run_all_unit_tests_main.c
@@ -1,10 +1,25 @@
+#ifndef RUN_ALL_UNIT_TESTS_MAIN_C
+#define RUN_ALL_UNIT_TESTS_MAIN_C
+
+/*
+ * run_all_unit_tests_main.c
+ * This file combines all the stand alone unit tests into a single set of unit tests.
+ */
+
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
+#include "error_reporting.h"
+#include "lexical_analyzer_unit_test_main.h"
+#include "parser_unit_test_main.h"
+#include "unit_test_logging.h"
 
 int main()
 {
     printf("Hello World!\n");
 }
 
-
+#endif // !RUN_ALL_UNIT_TESTS_MAIN_C
