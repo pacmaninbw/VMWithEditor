11573df2ed4fe1fcac5ecee1dbf2a50cfcc92f06
PAULSWINDOWS10\pacmaninbw <paul.chernick@chernicksw.com>
2020-09-12 15:19:43 -0700

    added internal_parser_tests.h so that the internal parser unit tests can be executed.

---
+++ VMWithEditor/UnitTests/Parser_Unit_Test/Parser_Unit_Test/internal_parser_tests.h
@@ -0,0 +1,13 @@
+#ifndef INTERNAL_PARSER_TESTS_H_
+#define INTERNAL_PARSER_TESTS_H_
+
+/*
+ * internal_parser_tests.h
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+bool run_all_internal_parser_unit_tests(size_t test_step);
+
+#endif	// !INTERNAL_PARSER_TESTS_H_
