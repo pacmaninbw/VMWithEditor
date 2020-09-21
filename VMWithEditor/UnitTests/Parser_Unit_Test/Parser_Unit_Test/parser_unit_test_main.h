26160fcea4debb40cf38b50569b4f0caa42d15ab
PAULSWINDOWS10\pacmaninbw <paul.chernick@chernicksw.com>
2020-09-21 12:07:25 -0700

    Added comments.

--- VMWithEditor/UnitTests/Parser_Unit_Test/Parser_Unit_Test/parser_unit_test_main.h
+++ VMWithEditor/UnitTests/Parser_Unit_Test/Parser_Unit_Test/parser_unit_test_main.h
@@ -1,6 +1,12 @@
 #ifndef PARSER_UNIT_TEST_MAIN_C
 #define PARSER_UNIT_TEST_MAIN_C
 
+/*
+ * parser_unit_test_main.h
+ *
+ * Provides a global entry point for the parser unit test to other applications.
+ */
+
 #include <stdbool.h>
 #include <stdint.h>
 
