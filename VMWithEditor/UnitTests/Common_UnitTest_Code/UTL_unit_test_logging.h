fcae3b3c6b93456f79aad36fa72146038607346d
PAULSWINDOWS10\pacmaninbw <paul.chernick@chernicksw.com>
2020-09-21 09:46:33 -0700

    Optimized logging by increasing the size of the output buffer used by fwrite and fprintf, replaced many fprintfs with fwrites. Added automatic indentation to unit test logging. Renamed UTL_create_and_init_test_log_data to UTL_new_log_data. Forced all logging to try to have a UTL_Test_Log_Data data structure, but there is a work around.

--- VMWithEditor/UnitTests/Common_UnitTest_Code/UTL_unit_test_logging.h
+++ VMWithEditor/UnitTests/Common_UnitTest_Code/UTL_unit_test_logging.h
@@ -13,39 +13,62 @@
 #include <stdio.h>
 
 #define UTL_LOG_BUFFER_SIZE 2048
+#define UTL_FWRITE_BUFFER_SIZE 16384
 
+/*
+ * Use these enums to indicate if the current testing includes errors or not,
+ * tests containing errors should use the UTL_NEGATIVE_PATH.
+ */
 typedef enum utl_path_state
 {
 	UTL_POSITIVE_PATH,
 	UTL_NEGATIVE_PATH
 } UTL_Path_State;
 
+/*
+ * This structure should be created for every unit test function for logging purposes
+ */
 typedef struct test_log_data
 {
 	const char* function_name;
 	UTL_Path_State path;
+	size_t indent_count;
 	bool status;
 	bool stand_alone;
 	bool internal_test;
-	bool not_used_yet;
+	bool use_indent;
 } UTL_Test_Log_Data;
 
+/*
+ * Unit test reporting functions.
+ */
 void UTL_log_test_status_each_step(const UTL_Test_Log_Data* test_data_to_log);
-void UTL_log_start_unit_test(const UTL_Test_Log_Data* log_data);
-void UTL_log_end_unit_test(const UTL_Test_Log_Data* log_data);
-void UTL_log_start_test_path(const UTL_Test_Log_Data* log_data);
-void UTL_log_end_test_path(const UTL_Test_Log_Data* log_data);
-void UTL_va_log_fprintf(const char* format, ...);
-void UTL_va_test_log_fprintf(const UTL_Test_Log_Data* log_data, bool print_header,
+void UTL_log_start_unit_test(UTL_Test_Log_Data* log_data, const char *addendum);
+void UTL_log_end_unit_test(UTL_Test_Log_Data* log_data, const char* addendum);
+void UTL_log_start_test_path(UTL_Test_Log_Data* log_data, const char* addendum);
+void UTL_log_end_test_path(UTL_Test_Log_Data* log_data, const char* addendum);
+void UTL_log_high_level_test_result(const UTL_Test_Log_Data* log_data,
+	const size_t test_id);
+void UTL_log_high_level_start_test(const UTL_Test_Log_Data* log_data,
+	const size_t test_id);
+void UTL_log_test_not_implemented(UTL_Test_Log_Data* log_data);
+/*
+ * Only use the following function if there are special needs for the test, if
+ * the code is repeated many times add an additional reporting function above.
+ */
+void UTL_va_test_log_formatted_output(const UTL_Test_Log_Data* log_data, bool print_header,
 	const char* format, ...);
 
+void UTL_report_create_and_init_test_log_data_memory_failure(const char* function_name);
+UTL_Test_Log_Data* UTL_new_log_data(const char* function_name, const bool status,
+	const UTL_Path_State path, const bool stand_alone, const bool internal_test);
+
+/*
+ * Unit test activation and deactivations called from unit test main functions.
+ * These functions should only be called once per unit test program execution.
+ */
 bool UTL_init_unit_tests(const char* log_file_name);
 void UTL_close_unit_tests(void);
-
-void UTL_report_create_and_init_test_log_data_memory_failure(const char* function_name);
-UTL_Test_Log_Data* UTL_create_and_init_test_log_data(const char* function_name,
-	const bool status, const UTL_Path_State path, const bool stand_alone,
-	const bool internal_test);
 
 #endif // !UTL_UNIT_TEST_LOGGING_H
 
--- VMWithEditor/UnitTests/Common_UnitTest_Code/unit_test_logging.c
+++ VMWithEditor/UnitTests/Common_UnitTest_Code/unit_test_logging.c
@@ -13,6 +13,39 @@
 #include "ERH_error_reporting.h"
 
 static FILE* UTL_unit_test_log_file = NULL;
+static char* UTL_fwrite_buffer = NULL;
+
+#define MAX_INDENT_BUFFER 64
+#define INDENT_TAB_SIZE 4
+#define WRITE_BUFFER_SIZE 4096
+
+typedef enum UTL_Start_End_Item_Type
+{
+	UTL_UNIT_TEST,
+	UTL_TEST_PATH
+} UTL_Start_End_Item_Type;
+
+static void indent_text_line(UTL_Test_Log_Data *log_data, char *out_buffer,
+	size_t out_buffer_size)
+{
+	char indent_buffer[MAX_INDENT_BUFFER] = { 0 };
+	if (log_data->use_indent && log_data->indent_count)
+	{
+		size_t indent_count = log_data->indent_count;
+		if ((INDENT_TAB_SIZE * indent_count + 1) < MAX_INDENT_BUFFER)
+		{
+			memset(indent_buffer, ' ', INDENT_TAB_SIZE * indent_count);
+			indent_buffer[INDENT_TAB_SIZE * indent_count] = (char)0;
+		}
+		else
+		{
+			memset(indent_buffer, ' ', MAX_INDENT_BUFFER);
+			indent_buffer[MAX_INDENT_BUFFER - 1] = (char)0;
+		}
+	}
+
+	SSF_strcat(out_buffer, indent_buffer, out_buffer_size);
+}
 
 static const char* get_path_string(const UTL_Test_Log_Data* log_data, const bool all_capitals)
 {
@@ -46,97 +79,193 @@ static const char* get_status_string(const UTL_Test_Log_Data* log_data, const bo
 	return status_string;
 }
 
-void UTL_va_log_fprintf(const char* format, ...)
+static void write_indented_line_to_log_file(UTL_Test_Log_Data* log_data,
+	char *out_buffer, size_t prepend_newline_count, size_t append_newline_count)
 {
-	va_list args;
-	va_start(args, format);
-	vfprintf(UTL_unit_test_log_file, format, args);
-	va_end(args);
+	char write_buffer[WRITE_BUFFER_SIZE] = { 0 };
 
-	fflush(ERH_error_out_file);
-}
-
-void UTL_va_test_log_fprintf(const UTL_Test_Log_Data* log_data, bool print_header, const char* format, ...)
-{
-	if (log_data->stand_alone)
+	for (size_t i = 0; i < prepend_newline_count; i++)
 	{
-		if (print_header) {
-			fprintf(UTL_unit_test_log_file, "Unit test function %s: %s path: ",
-				log_data->function_name, get_path_string(log_data, true));
-		}
-		va_list args;
-		va_start(args, format);
-		vfprintf(UTL_unit_test_log_file, format, args);
-		va_end(args);
+		SSF_strcat(write_buffer, "\n", WRITE_BUFFER_SIZE);
 	}
-}
+	indent_text_line(log_data, write_buffer, sizeof(write_buffer));
 
-void UTL_log_start_unit_test(const UTL_Test_Log_Data* log_data)
-{
-	if (log_data->internal_test)
+	if (strlen(write_buffer) + strlen(out_buffer) + 1 < WRITE_BUFFER_SIZE)
 	{
-		if (log_data->stand_alone)
+		SSF_strcat(write_buffer, out_buffer, sizeof(write_buffer));
+
+		for (size_t i = 0; i < append_newline_count; i++)
 		{
-			fprintf(UTL_unit_test_log_file, "STARTING internal unit test for %s\n\n", log_data->function_name);
+			SSF_strcat(write_buffer, "\n", WRITE_BUFFER_SIZE);
 		}
+
+		fwrite(write_buffer, sizeof(*write_buffer), strlen(write_buffer),
+			UTL_unit_test_log_file);
 	}
 	else
 	{
-		fprintf(UTL_unit_test_log_file, "STARTING unit test for %s\n\n", log_data->function_name);
+		fwrite(out_buffer, sizeof(*out_buffer), strlen(out_buffer),
+			UTL_unit_test_log_file);
 	}
 }
 
-void UTL_log_end_unit_test(const UTL_Test_Log_Data* log_data)
+/*
+ * No indentation necessary. This will alway be out put.
+ */
+void UTL_log_high_level_test_result(const UTL_Test_Log_Data* log_data,
+	const size_t test_id)
 {
-	if (log_data->internal_test)
+	fprintf(UTL_unit_test_log_file,"\nUnit Test %zd: %s : %s\n\n", test_id,
+		log_data->function_name, get_status_string(log_data, false));
+}
+
+void UTL_log_high_level_start_test(const UTL_Test_Log_Data* log_data,
+	const size_t test_id)
+{
+	fprintf(UTL_unit_test_log_file, "Unit Test %zd: Starting %s\n\n", test_id,
+		log_data->function_name);
+}
+
+void UTL_log_test_not_implemented(UTL_Test_Log_Data* log_data)
+{
+	char out_buffer[WRITE_BUFFER_SIZE] = { 0 };
+	snprintf(out_buffer, sizeof(out_buffer), "Unit test: %s NOT IMPLEMENTED\n",
+		log_data->function_name);
+	size_t prepend_newline_count = 1;
+	size_t append_newline_count = 0;
+
+	write_indented_line_to_log_file(log_data, out_buffer, prepend_newline_count,
+		append_newline_count);
+
+	log_data->status = false;
+}
+
+static void generic_start_test_log_item(UTL_Test_Log_Data* log_data,
+	const char* additional_data, UTL_Start_End_Item_Type test_type)
+{
+	char out_buffer[WRITE_BUFFER_SIZE] = { 0 };
+	const char* addendum = (additional_data) ? additional_data : "";
+	size_t prepend_newline_count = 0;
+	size_t append_newline_count = 0;
+
+	if (test_type == UTL_UNIT_TEST)
 	{
-		if (log_data->stand_alone)
-		{
-			fprintf(UTL_unit_test_log_file, "\nENDING internal unit test for %s: test %s\n\n",
-				log_data->function_name, get_status_string(log_data, true));
-		}
+		snprintf(out_buffer, sizeof(out_buffer), "STARTING%sunit test for %s %s\n\n",
+			log_data->internal_test ? " internal " : " ",
+			log_data->function_name, addendum);
 	}
 	else
 	{
-		fprintf(UTL_unit_test_log_file, "\nENDING unit test for %s: test %s\n\n",
-			log_data->function_name, get_status_string(log_data, true));
+		snprintf(out_buffer, sizeof(out_buffer), "Starting %s PATH testing for %s %s\n\n",
+			get_path_string(log_data, true), log_data->function_name, addendum);
+		prepend_newline_count = 1;
 	}
+
+	write_indented_line_to_log_file(log_data, out_buffer, prepend_newline_count,
+		append_newline_count);
+
+	if (log_data->use_indent)
+	{
+		log_data->indent_count++;
+	}
+}
+
+void UTL_log_start_unit_test(UTL_Test_Log_Data* log_data,
+	const char *additional_data)
+{
+	if (log_data->internal_test && !log_data->stand_alone)
+	{
+		return;
+	}
+
+	generic_start_test_log_item(log_data, additional_data, UTL_UNIT_TEST);
+}
+
+void UTL_log_start_test_path(UTL_Test_Log_Data* log_data,
+	const char* additional_data)
+{
+	// A test path is internal by definition
+	if (!log_data->stand_alone)
+	{
+		return;
+	}
+
+	generic_start_test_log_item(log_data, additional_data, UTL_TEST_PATH);
+}
+
+static void generic_end_test_log_item(UTL_Test_Log_Data* log_data,
+	const char* additional_data, UTL_Start_End_Item_Type test_type)
+{
+	if (log_data->use_indent && log_data->indent_count)
+	{
+		log_data->indent_count--;
+	}
+
+	char out_buffer[WRITE_BUFFER_SIZE] = { 0 };
+	const char* addendum = (additional_data) ? additional_data : "";
+	size_t prepend_newline_count = 1;
+	size_t append_newline_count = 0;
+
+	if (test_type == UTL_UNIT_TEST)
+	{
+		snprintf(out_buffer, sizeof(out_buffer),
+			"ENDING unit test for %s: test %s %s\n\n",
+			log_data->function_name, get_status_string(log_data, true),
+			addendum);
+	}
+	else
+	{
+		const char* path = get_path_string(log_data, true);
+		snprintf(out_buffer, sizeof(out_buffer),
+			"Ending %s PATH testing for %s, %s PATH  %s %s\n\n",
+			path, log_data->function_name, path,
+			get_status_string(log_data, true), addendum);
+	}
+
+	write_indented_line_to_log_file(log_data, out_buffer, prepend_newline_count,
+		append_newline_count);
+}
+
+void UTL_log_end_test_path(UTL_Test_Log_Data *log_data,
+	const char* additional_data)
+{
+	if (!log_data->stand_alone)
+	{
+		return;
+	}
+
+	generic_end_test_log_item(log_data, additional_data, UTL_TEST_PATH);
+}
+
+void UTL_log_end_unit_test(UTL_Test_Log_Data* log_data,
+	const char* additional_data)
+{
+	if (log_data->internal_test && !log_data->stand_alone)
+	{
+		return;
+	}
+
+	generic_end_test_log_item(log_data, additional_data, UTL_UNIT_TEST);
 
 	fflush(UTL_unit_test_log_file);		// Current unit test is done flush the output.
 }
 
-void UTL_log_test_status_each_step(const UTL_Test_Log_Data *log_data)
+void UTL_log_test_status_each_step(const UTL_Test_Log_Data* log_data)
 {
-	if (log_data->stand_alone)
+	if (!log_data->stand_alone)
 	{
-		fprintf(UTL_unit_test_log_file, "%s(): %s Path %s\n", log_data->function_name,
-			get_path_string(log_data, false), get_status_string(log_data, false));
-	}
-}
-
-void UTL_log_start_test_path(const UTL_Test_Log_Data* log_data)
-{
-	if (log_data->stand_alone)
-	{
-		fprintf(UTL_unit_test_log_file, "\nStarting %s PATH testing for %s\n\n", 
-			get_path_string(log_data, true), log_data->function_name);
-	}
-}
-
-void UTL_log_end_test_path(const UTL_Test_Log_Data *log_data)
-{
-	const char* path = get_path_string(log_data, true);
-
-	if (log_data->stand_alone)
-	{
-		fprintf(UTL_unit_test_log_file, "\nEnding %s PATH testing for %s, %s PATH  %s\n\n",
-			path, log_data->function_name, path, get_status_string(log_data, true));
+		return;
 	}
 
-	if (log_data->path == UTL_NEGATIVE_PATH)
-	{
-		fflush(UTL_unit_test_log_file);		// Current unit test is done flush the output.
-	}
+	char out_buffer[WRITE_BUFFER_SIZE];
+	size_t prepend_newline_count = 0;
+	size_t append_newline_count = 0;
+
+	snprintf(out_buffer, sizeof(out_buffer), "%s(): %s Path %s\n", log_data->function_name,
+		get_path_string(log_data, false), get_status_string(log_data, false));
+
+	write_indented_line_to_log_file((UTL_Test_Log_Data*)log_data, out_buffer, 
+		prepend_newline_count, append_newline_count);
 }
 
 // provides common error report for memory allocation error.
@@ -146,14 +275,7 @@ void UTL_report_create_and_init_test_log_data_memory_failure(const char *functio
 	snprintf(buffer, sizeof(buffer),
 		"In function %s, Memory allocation failed in create_and_init_test_log_data\n",
 		function_name);
-	if (errno)
-	{
-		perror(buffer);
-	}
-	else
-	{
-		fprintf(ERH_error_out_file, "%s\n", buffer);
-	}
+	ERH_use_perror_when_errno(buffer);
 }
 
 bool UTL_init_unit_tests(const char* log_file_name)
@@ -172,6 +294,22 @@ bool UTL_init_unit_tests(const char* log_file_name)
 			ERH_report_error_output_fopen_failed(log_file_name);
 			return false;
 		}
+		else
+		{
+			UTL_fwrite_buffer = malloc(UTL_FWRITE_BUFFER_SIZE);
+			if (!UTL_fwrite_buffer)
+			{
+				char *malloc_error =
+					"In UTL_init_unit_tests malloc() of UTL_fwrite_buffer failed";
+				ERH_use_perror_when_errno(malloc_error);
+				return false;
+			}
+			if (setvbuf(UTL_unit_test_log_file, NULL, _IOFBF, UTL_FWRITE_BUFFER_SIZE)) {
+				char* setvbuf_error = "In UTL_init_unit_tests setvbuf() of UTL_fwrite_buffer failed";
+				ERH_use_perror_when_errno(setvbuf_error);
+				return false;
+			}
+		}
 		ERH_error_out_file = UTL_unit_test_log_file;
 	}
 	else
@@ -188,10 +326,11 @@ void UTL_close_unit_tests(void)
 	if (UTL_unit_test_log_file != stdout && UTL_unit_test_log_file != stderr)
 	{
 		fclose(UTL_unit_test_log_file);
+		free(UTL_fwrite_buffer);
 	}
 }
 
-UTL_Test_Log_Data* UTL_create_and_init_test_log_data(const char* function_name,
+UTL_Test_Log_Data* UTL_new_log_data(const char* function_name,
 	const bool status, const UTL_Path_State path, const bool stand_alone,
 	const bool internal_test)
 {
@@ -203,22 +342,37 @@ UTL_Test_Log_Data* UTL_create_and_init_test_log_data(const char* function_name,
 		log_data->path = path;
 		log_data->stand_alone = stand_alone;
 		log_data->internal_test = internal_test;
+		log_data->use_indent = true;
 	}
 	else
 	{
 		char buffer[ERH_ERROR_BUFFER_SIZE];
 		snprintf(buffer, sizeof(buffer), "In %s calloc() failed", "create_and_init_test_log_data");
-		if (errno)
-		{
-			perror(buffer);
-		}
-		else
-		{
-			fprintf(ERH_error_out_file, "%s\n", buffer);
-		}
+		ERH_use_perror_when_errno(buffer);
 	}
 
 	return log_data;
+}
+
+void UTL_va_test_log_formatted_output(const UTL_Test_Log_Data* log_data, bool print_header, const char* format, ...)
+{
+	bool stand_alone = true;
+	if (log_data)
+	{
+		stand_alone = log_data->stand_alone;
+		if (print_header && stand_alone) {
+			fprintf(UTL_unit_test_log_file, "Unit test function %s: %s path: ",
+				log_data->function_name, get_path_string(log_data, true));
+		}
+	}
+
+	if (stand_alone)
+	{
+		va_list args;
+		va_start(args, format);
+		vfprintf(UTL_unit_test_log_file, format, args);
+		va_end(args);
+	}
 }
 
 #endif	// !UNIT_TEST_LOGGING_C
