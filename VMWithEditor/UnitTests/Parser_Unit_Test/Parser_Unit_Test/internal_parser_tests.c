bec8c3f539090e8bc6d4ea334b133fb0fb6e0c65
pacmaninbw <paul.chernick@chernicksw.com>
2020-11-17 13:06:20 -0800

    Changed unit test interfaces to allow layered unit test reporting.

--- VMWithEditor/UnitTests/Parser_Unit_Test/Parser_Unit_Test/internal_parser_tests.c
+++ VMWithEditor/UnitTests/Parser_Unit_Test/Parser_Unit_Test/internal_parser_tests.c
@@ -24,12 +24,12 @@
  */
 #include "parser.c"
 
-static bool unit_test_get_opcode_or_operand_string(size_t test_step)
+static bool unit_test_get_opcode_or_operand_string(UTL_Test_Log_Data* log_data)
 {
 	bool passed = true;
-	UTL_Test_Log_Data *log_data = UTL_new_log_data(
-		"unit_test_get_opcode_or_operand_string", passed, UTL_POSITIVE_PATH,
-		test_step == 0, true);
+	log_data->function_name = "unit_test_get_opcode_or_operand_string";
+	log_data->status = passed;
+	log_data->path = UTL_POSITIVE_PATH;
 
 	UTL_log_start_unit_test(log_data, NULL);
 
@@ -46,12 +46,12 @@ static bool unit_test_get_opcode_or_operand_string(size_t test_step)
 	return passed;
 }
 
-static bool unit_test_get_legal_opcode_or_oparand(size_t test_step)
+static bool unit_test_get_legal_opcode_or_oparand(UTL_Test_Log_Data* log_data)
 {
 	bool passed = true;
-	UTL_Test_Log_Data* log_data = UTL_new_log_data(
-		"unit_test_get_legal_opcode_or_oparand", passed, UTL_POSITIVE_PATH,
-		test_step == 0, true);
+	log_data->function_name = "unit_test_get_legal_opcode_or_oparand";
+	log_data->status = passed;
+	log_data->path = UTL_POSITIVE_PATH;
 
 	UTL_log_start_unit_test(log_data, NULL);
 
@@ -68,15 +68,16 @@ static bool unit_test_get_legal_opcode_or_oparand(size_t test_step)
 	return passed;
 }
 
-static bool unit_test_print_syntax_errors(size_t test_step)
+static bool unit_test_print_syntax_errors(UTL_Test_Log_Data* log_data)
 {
 	bool passed = true;
 	unsigned syntax_check_list[LAH_SYNTAX_CHECK_ARRAY_SIZE];
 	char* test_file_name = "No File Name";
 	char* text_line = "Unit Testing print_syntax_errors()";
 	bool this_test_passed = true;
-	UTL_Test_Log_Data* log_data = UTL_new_log_data(
-		"unit_test_print_syntax_errors", passed, UTL_POSITIVE_PATH, (!test_step), true);
+	log_data->function_name = "unit_test_print_syntax_errors";
+	log_data->status = passed;
+	log_data->path = UTL_POSITIVE_PATH;
 	size_t line_number = 1;
 
 	UTL_log_start_unit_test(log_data, NULL);
@@ -125,7 +126,7 @@ static bool unit_test_print_syntax_errors(size_t test_step)
 	return passed;
 }
 
-typedef bool (*parser_unit_test_function)(size_t test_step);
+typedef bool (*parser_unit_test_function)(UTL_Test_Log_Data* log_data);
 
 typedef struct unit_test_functions_and_args
 {
@@ -134,10 +135,9 @@ typedef struct unit_test_functions_and_args
 } Parser_Unit_Test_Functions;
 
 
-bool run_all_internal_parser_unit_tests(size_t test_step)
+bool run_all_internal_parser_unit_tests(UTL_Test_Log_Data* log_data)
 {
 	bool all_tests_passed = true;
-	UTL_Test_Log_Data* log_data = UTL_new_log_data("run_all_internal_parser_unit_tests", all_tests_passed, UTL_POSITIVE_PATH, test_step == 0, true);
 
 	Parser_Unit_Test_Functions unit_tests[] =
 	{
@@ -150,10 +150,12 @@ bool run_all_internal_parser_unit_tests(size_t test_step)
 	};
 	size_t test_max = (sizeof(unit_tests) / sizeof(*unit_tests));
 
+	UTL_log_start_unit_test(log_data, NULL);
+
 	const char* overall_log_function_name = log_data->function_name;
 	for (size_t test_count = 0; test_count < test_max; test_count++)
 	{
-		bool test_passed = unit_tests[test_count].func(test_step);
+		bool test_passed = unit_tests[test_count].func(log_data);
 		log_data->function_name = unit_tests[test_count].test_name;
 		log_data->status = test_passed;
 		UTL_log_high_level_test_result(log_data, test_count + 1);
@@ -163,6 +165,10 @@ bool run_all_internal_parser_unit_tests(size_t test_step)
 			all_tests_passed = test_passed;
 		}
 	}
+
+	log_data->function_name = overall_log_function_name;
+	log_data->status = all_tests_passed;
+	UTL_log_end_unit_test(log_data, NULL);
 
 	return all_tests_passed;
 }
--- VMWithEditor/UnitTests/Parser_Unit_Test/Parser_Unit_Test/internal_parser_tests.h
+++ VMWithEditor/UnitTests/Parser_Unit_Test/Parser_Unit_Test/internal_parser_tests.h
@@ -7,7 +7,8 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <UTL_unit_test_logging.h>
 
-bool run_all_internal_parser_unit_tests(size_t test_step);
+bool run_all_internal_parser_unit_tests(UTL_Test_Log_Data* log_data);
 
 #endif	// !INTERNAL_PARSER_TESTS_H_
