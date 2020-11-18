f159311f3d1ca423c5c42f36dbfe3162303137fb
pacmaninbw <paul.chernick@chernicksw.com>
2020-11-17 16:30:54 -0800

    Update internal_parser_tests.c

--- VMWithEditor/UnitTests/Parser_Unit_Test/Parser_Unit_Test/internal_parser_tests.c
+++ VMWithEditor/UnitTests/Parser_Unit_Test/Parser_Unit_Test/internal_parser_tests.c
@@ -18,11 +18,55 @@
 #include "LAH_lexical_analyzer.h"
 #include "UTL_unit_test_logging.h"
 #include "internal_parser_tests.h"
+#include "OPC_opcode.h"
 
 /*
  * Include parser.c so we can test the internal static functions in the file.
  */
 #include "parser.c"
+
+/*
+ * This struct is used to test both get_opcode_or_operand_string() and
+ * get_legal_opcode_or_oparand().
+ */
+typedef struct {
+	UTL_Path_State test_path;
+	LAH_Syntax_State state;
+	size_t expected_opcode_or_operand;
+	size_t source_string_length;
+	char* source_test_string;
+	char* expected_string;
+} GOOS_And_GLOO_Test_Data;
+
+static GOOS_And_GLOO_Test_Data goos_and_gloo_test_data[] = 
+{
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_HALT, sizeof("HALT"), "HALT", "HALT"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_PUSH, sizeof("PUSH"), "PUSH", "PUSH"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_POP, sizeof("POP"), "POP", "POP"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_STORE, sizeof("STORE"), "STORE", "STORE"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_LOAD, sizeof("LOAD"), "LOAD", "LOAD"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_ADD, sizeof("ADD"), "ADD", "ADD"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_SUBTRACT, sizeof("SUBTRACT"), "SUBTRACT", "SUBTRACT"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_MULTIPLY, sizeof("MULTIPLY"), "MULTIPLY", "MULTIPLY"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_DIVIDE, sizeof("DIVIDE"), "DIVIDE", "DIVIDE"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_OUTPUTCHAR, sizeof("OUTPUTCHAR"), "OUTPUTCHAR", "OUTPUTCHAR"},
+	{UTL_POSITIVE_PATH, LAH_OPCODE_STATE, OPC_INPUTCHAR, sizeof("INPUTCHAR"), "INPUTCHAR", "INPUTCHAR"},
+	{UTL_POSITIVE_PATH, LAH_OPERAND_STATE, 0x00, sizeof("0x00"), "0x00", "0x00"},		// Minimum legal value
+	{UTL_POSITIVE_PATH, LAH_OPERAND_STATE, 0xFFFFFF, sizeof("0xFFFFFF"), "0xFFFFFF", "0xFFFFFF"},	// Maximum legal value
+	{UTL_POSITIVE_PATH, LAH_OPERAND_STATE, 0xFFFFFE, sizeof("0xFFFFFE"), "0xFFFFFE", "0xFFFFFE"},
+	{UTL_POSITIVE_PATH, LAH_OPERAND_STATE, 0x01, sizeof("0x01"), "0x01", "0x01"},
+	{UTL_POSITIVE_PATH, LAH_OPERAND_STATE, 0x000FFF, sizeof("0x000FFF"), "0x000FFF", "0x000FFF"},
+/* 
+ *End Positive Test Path
+ */
+	{UTL_NEGATIVE_PATH, LAH_OPCODE_STATE, (size_t)-1, sizeof("HEALT"), "HEALT", "HEALT"},	// Passes GOOS fails GLOO, invalid opcode
+	{UTL_NEGATIVE_PATH, LAH_OPCODE_STATE, (size_t)-1, 0, "HEALT", NULL},					// Fails Both GOOS and GLOO string length zero
+	{UTL_NEGATIVE_PATH, LAH_OPCODE_STATE, (size_t)-1, sizeof("HEALT"), NULL, NULL}, 		// Fails Both GOOS and GLOO NULL input string
+	{UTL_NEGATIVE_PATH, LAH_OPCODE_STATE, (size_t)-1, 0, NULL, NULL}, 						// Fails Both GOOS and GLOO
+	{UTL_NEGATIVE_PATH, LAH_OPCODE_STATE, (size_t)-1, sizeof("0x00"), "0x00", "0x00"},		// Passes GOOS fails GLOO, invalid opcode
+	{UTL_NEGATIVE_PATH, LAH_OPERAND_STATE, (size_t)-1, sizeof("HALT"), "HALT", "HALT"},		// Passes GOOS fails GLOO, not numeric
+	{UTL_NEGATIVE_PATH, LAH_OPERAND_STATE, (size_t)-1, sizeof("0xFFFFFFFF"), "0xFFFFFFFF", "0xFFFFFFFF"},	// Maximum legal value exceded
+};
 
 static bool unit_test_get_opcode_or_operand_string(UTL_Test_Log_Data* log_data)
 {
