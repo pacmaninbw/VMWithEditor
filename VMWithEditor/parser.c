68a0092beb95873304aa1a6dd91c013d1911b80f
pacmaninbw <paul.chernick@chernicksw.com>
2020-11-25 11:01:28 -0800

    Parser unit tests complete, parser passes unit tests.

--- VMWithEditor/parser.c
+++ VMWithEditor/parser.c
@@ -6,12 +6,12 @@
 #ifndef PARSER_C
 #define PARSER_C
 
-#include <errno.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 #include "vmwitheditor.h"
 #include "ERH_error_reporting.h"
@@ -32,8 +32,11 @@ static void init_error_strings(const char* error_strings[LAH_SYNTAX_CHECK_ARRAY_
 	error_strings[LAH_LEGALOPCODE] = "Missing or unknow opcode";
 	error_strings[LAH_LEGALOPERAND] = "Missing operand or operand out of range";
 	error_strings[LAH_ILLEGALOPCODE] = "Unknown Opcode.";
-	error_strings[LAH_ILLEGALFIRSTCHAR] = "Illegal character in column 1 (are you missing the opening brace { )";
+	error_strings[LAH_ILLEGALFIRSTCHAR] =
+		"Illegal character in column 1 (are you missing the opening brace { )";
 	error_strings[LAH_MULTIPLESTATEMENTSONELINE] = "Only one program step per line";
+	error_strings[LAH_MISSINGCOMMA] = "Missing comma(s)";
+	error_strings[LAH_ILLEGALCHAR] = "Illegal character";
 }
 
 static bool print_syntax_errors(const unsigned* necessary_items, size_t* line_number,
@@ -45,20 +48,25 @@ static bool print_syntax_errors(const unsigned* necessary_items, size_t* line_nu
 	char* error_strings[LAH_SYNTAX_CHECK_ARRAY_SIZE];
 	init_error_strings(error_strings);
 	char illegal_operand[256];
-	snprintf(illegal_operand, sizeof(illegal_operand), "The operand is out of range [0x0, 0x%zx]", VMH_get_maximum_operand_value());
+	snprintf(illegal_operand, sizeof(illegal_operand),
+		"The operand is out of range [0x0, 0x%zx]",
+		VMH_get_maximum_operand_value());
 	error_strings[LAH_ILLEGALOPERAND] = illegal_operand;
 
 	for (size_t i = 0; i < LAH_SYNTAX_CHECK_ARRAY_SIZE; i++)
 	{
-		error_count += (!necessary_items[i] && i < LAH_ILLEGALOPCODE) ? 1 : ((i >= LAH_ILLEGALOPCODE && necessary_items[i]) ? 1 : 0);
+		error_count += (!necessary_items[i] && i < LAH_ILLEGALOPCODE) ? 1 :
+			((i >= LAH_ILLEGALOPCODE && necessary_items[i]) ? 1 : 0);
 	}
 
 	if (error_count)
 	{
 		ERH_va_report_error_fprintf("In file %s line %zu there are %u errors: %s\n",
 			file_name, *line_number, error_count, line);
-		ERH_va_report_error_fprintf("    The proper format of a program step is { OPCODE , OPERAND }[,]"
-			" the second comma is necessary when the program step is not the last one in the file. ");
+		ERH_va_report_error_fprintf(
+			"    The proper format of a program step is { OPCODE , OPERAND }[,]"
+			" the second comma is necessary when the program step is not the "
+			"last one in the file. ");
 		ERH_va_report_error_fprintf("Only one program step per line\n");
 		syntax_is_good = false;
 		if (!necessary_items[LAH_OPENBRACE])		// Prevent cascading error messages that may not be correct
@@ -85,73 +93,103 @@ static bool print_syntax_errors(const unsigned* necessary_items, size_t* line_nu
 	return syntax_is_good;
 }
 
-static unsigned char* get_opcode_or_operand_string(Const_U_Char* original)
+static void get_opcode_or_operand_string(Const_U_Char* original, size_t length, unsigned char* possible_op)
 {
-	unsigned char* local_copy = SSF_ucstrdup(original);
-	if (!local_copy)
+	char* functtion_name = "get_opcode_or_operand_string";
+	if (original && length)
 	{
-		return local_copy;
-	}
-	register unsigned char* current_char = local_copy;
-	unsigned char* return_string = NULL;
-
-	while (isalnum(*current_char))
-	{
-		current_char++;
-	}
-
-	*current_char = '\0';
-
-	return_string = SSF_ucstrdup(local_copy);
-	free(local_copy);
-
-	return return_string;
-}
-
-static unsigned get_legal_opcode_or_oparand(Const_U_Char** current_character,
-	unsigned syntax_check_list[], const LAH_Syntax_Check_List_Items prime_index)
-{
-	if (prime_index != LAH_LEGALOPCODE && prime_index != LAH_LEGALOPERAND)
-	{
-		ERH_va_report_error_fprintf(
-			"\n\n*** Illegal prime_index in get_legal_opcode_or_oparand()\n\n ***");
-		return (unsigned)-1;
-	}
-
-	unsigned char* possible_op = get_opcode_or_operand_string(*current_character);
-	if (!possible_op)
-	{
-		char *err_msg = "get_opcode_or_operand_string: Failed: returned NULL";
-		if (errno)
-		{
-			perror(err_msg);
-		}
-		else
-		{
-			ERH_va_report_error_fprintf("%s\n", err_msg);
-		}
-		return (unsigned)-1;
-	}
-
-	int test_value = (prime_index == LAH_LEGALOPCODE) ? OPC_translate_string_to_opcode(possible_op) :
-		VMH_translate_text_to_operand_and_validate((char*)possible_op);
-	int minimum_value = (prime_index == LAH_LEGALOPCODE) ? (int)OPC_HALT : 0;
-
-	if (test_value < minimum_value)
-	{
-		syntax_check_list[prime_index] = 0;
-		syntax_check_list[(prime_index == LAH_LEGALOPCODE) ? LAH_ILLEGALOPCODE :
-			LAH_ILLEGALOPERAND]++;
+		strncpy((char*)possible_op, (char*)original, length);
 	}
 	else
 	{
-		syntax_check_list[prime_index]++;
+		char pointer_numeric_value[ERH_ERROR_BUFFER_SIZE];
+		snprintf(pointer_numeric_value, sizeof(pointer_numeric_value),
+			" original = 0x%" PRIXPTR, (uintptr_t)original);
+		ERH_va_report_error_fprintf(
+			"Internal error: %s: illegal value opcode or operand string %s lenght = %zu\n",
+			functtion_name, pointer_numeric_value, length);
+	}
+}
+
+/*
+ * Returns the value of the opcode or operand is valid, -1 otherwise. Updates
+ * the syntax item check list.
+ */
+static int validate_opcode_or_operand(Const_U_Char *possible_op,
+	const LAH_Syntax_State state, unsigned syntax_check_list[])
+{
+	int test_value = -1;
+	LAH_Syntax_Check_List_Items legal_index = LAH_OPENBRACE;
+	LAH_Syntax_Check_List_Items illegal_index = LAH_OPENBRACE;
+
+	if (state == LAH_OPCODE_STATE)
+	{
+		test_value = OPC_translate_string_to_opcode(possible_op);
+		legal_index = LAH_LEGALOPCODE;
+		illegal_index = LAH_ILLEGALOPCODE;
+	}
+	else if (state == LAH_OPERAND_STATE)
+	{
+		test_value = VMH_translate_text_to_operand_and_validate((char*)possible_op);
+		legal_index = LAH_LEGALOPERAND;
+		illegal_index = LAH_ILLEGALOPERAND;
+	}
+	else
+	{
+		return test_value;
 	}
 
-	*current_character += (strlen((char*)possible_op) - 1);		// advance the current character past the string processed.
-	free(possible_op);											// No memory leaks allocated in get_opcode_or_operand_string()
+	if (test_value < 0)
+	{
+		syntax_check_list[legal_index] = 0;
+		syntax_check_list[illegal_index]++;
+	}
+	else
+	{
+		syntax_check_list[legal_index]++;
+	}
 
-	return (unsigned)minimum_value;
+	return test_value;
+}
+
+#define MAX_OP_LENGTH 64
+/* 
+ * Returns an opcode or operand if the input string value is legal, (unsigned) -1
+ * otherwise. Updates the syntax item check list as necessary.
+ */
+static unsigned int get_legal_opcode_or_oparand(Const_U_Char* current_character,
+	const size_t possible_op_length, unsigned syntax_check_list[],
+	const LAH_Syntax_State state)
+{
+	unsigned int return_val = (unsigned int)-1;
+	if (state != LAH_OPCODE_STATE && state != LAH_OPERAND_STATE)
+	{
+		ERH_report_error_generic(
+			"\nInternal error: get_legal_opcode_or_oparand() : Illegal state.\n");
+	}
+	else
+	{
+		if (possible_op_length < MAX_OP_LENGTH)
+		{
+			unsigned char possible_op[MAX_OP_LENGTH];
+			memset(possible_op, 0, sizeof(possible_op));
+			get_opcode_or_operand_string(current_character, possible_op_length,
+				possible_op);
+			if (strlen((char *)possible_op))
+			{
+				return_val = validate_opcode_or_operand(possible_op, state,
+					syntax_check_list);
+			}
+		}
+		else
+		{
+			ERH_va_report_error_fprintf(
+				"Internal error: %s : string length %zu greater than buffer size %d\n",
+				"get_legal_opcode_or_oparand", possible_op_length, MAX_OP_LENGTH);
+		}
+	}
+
+	return return_val;
 }
 
 static void check_for_required_character(Const_U_Char current_character,
@@ -180,7 +218,6 @@ static void check_for_required_character(Const_U_Char current_character,
 			syntax_check_list[LAH_MULTIPLESTATEMENTSONELINE]++;
 		}
 	}
-
 }
 
 /*
@@ -198,11 +235,8 @@ void deactivate_parser(void)
 	deactivate_lexical_analyzer();
 }
 
-/* Expected syntax:		{opcode, operand} with possible comma at the end.
- *
- * To prevent the global function parser from being defined in multiple object
- * file when this file is included for unit test we check to see if we are in
- * the internal parser unit test file.
+/*
+ * Expected syntax:		{opcode, operand} with possible comma at the end.
  */
 HRF_Program_Step_Node* parser(Const_U_Char* text_line, size_t* line_number, const char* file_name)
 {
@@ -210,6 +244,8 @@ HRF_Program_Step_Node* parser(Const_U_Char* text_line, size_t* line_number, cons
 	memset(&syntax_check_list[0], 0, sizeof(syntax_check_list));
 	HRF_Human_Readable_Program_Format legal = { OPC_OPCODE_TRANSLATOR_ARRAY_SIZE, 0 };
 	LAH_Syntax_State current_state = LAH_START_STATE;
+	size_t possible_op_char_count = 0;
+	Const_U_Char* possible_op = NULL;
 
 	Const_U_Char* current_character = text_line;
 	while (*current_character)
@@ -217,17 +253,41 @@ HRF_Program_Step_Node* parser(Const_U_Char* text_line, size_t* line_number, cons
 		LAH_Syntax_State new_state = lexical_analyzer(current_state, *current_character, syntax_check_list);
 		if (new_state != current_state)
 		{
-			//TODO Correct state transition testing
-			if (current_state == LAH_ENTER_OPCODE_STATE && isalpha(*current_character))
+			if (new_state == LAH_OPCODE_STATE)
 			{
-				legal.opcode = get_legal_opcode_or_oparand(&current_character, syntax_check_list, LAH_LEGALOPCODE);
+				possible_op = current_character;
+				possible_op_char_count = 1;
 			}
 
-			if (syntax_check_list[(size_t)LAH_OPENBRACE] && syntax_check_list[(size_t)LAH_COMMA] && isalnum(*current_character))
+			if (new_state == LAH_OPERAND_STATE)
 			{
-				legal.operand = get_legal_opcode_or_oparand(&current_character, syntax_check_list, LAH_LEGALOPERAND);
+				possible_op = current_character;
+				possible_op_char_count = 1;
+			}
+			//TODO Correct state transition testing
+			if (current_state == LAH_OPCODE_STATE)
+			{
+   				legal.opcode = get_legal_opcode_or_oparand(possible_op, possible_op_char_count, syntax_check_list, current_state);
+				possible_op_char_count = 0;
+			}
+
+			if (current_state == LAH_OPERAND_STATE)
+			{
+				if (!syntax_check_list[LAH_COMMA])
+				{
+					syntax_check_list[LAH_MISSINGCOMMA]++;
+				}
+				legal.operand = get_legal_opcode_or_oparand(possible_op, possible_op_char_count, syntax_check_list, current_state);
+				possible_op_char_count = 0;
 			}
 			current_state = new_state;
+		}
+		else
+		{
+			if (new_state == LAH_OPCODE_STATE || new_state == LAH_OPERAND_STATE)
+			{
+				possible_op_char_count++;
+			}
 		}
 
 		current_character++;
